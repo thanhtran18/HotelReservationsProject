eb566723cfa4e2927982236bfcea993fe581a763
Thanh Tran (Kevin) <tranct@myumanitoba.ca>
2017-09-22 21:55:06 -0500

    Add files via upload

---
+++ ListItem.java
@@ -0,0 +1,14 @@
+//-----------------------------------------
+// CLASS: ListItem
+//
+// Author: Cong Thanh Tran
+//
+// REMARKS: An abstract class defining the group of things that could appear
+//			as data in a SimpleLinkedList.  
+//
+//-----------------------------------------
+
+public abstract class ListItem
+{
+    
+} //ListItem
